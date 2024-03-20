c22cbe4ab2c45f17a8e97ed0bc8680a4d5066af7
sumitMahamuni <sumitmahamuni8@gmail.com>
2024-03-20 18:52:04 +0530

    Test driven Development(TDD) added deserialized and serialized test cases.

---
+++ cashcard/src/main/java/hazelTech/cashcard/CashCard.java
@@ -0,0 +1,5 @@
+package hazelTech.cashcard;
+
+record CashCard(Long id, Double amount) {
+	
+}
---
+++ cashcard/src/test/java/hazelTech/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package hazelTech.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
