f7e71b742a84495fe2514d7c8f5efbd433607a92
sumitMahamuni <sumitmahamuni8@gmail.com>
2024-03-22 19:23:42 +0530

    Added repository layer and configured H2 database

--- cashcard/src/main/java/hazelTech/cashcard/CashCardController.java
+++ cashcard/src/main/java/hazelTech/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package hazelTech.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 public class CashCardController {
 	
+	private final CashCardRepository cashCardRepository;
+	
+	private CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
+	
 	@GetMapping("/{requestedId}")
 	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
 		
-		if(requestedId.equals(99L)) {
-			
-			CashCard cashcard = new CashCard(99L, 123.45);
-			
-			return ResponseEntity.ok(cashcard);
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+		
+		if(cashCardOptional.isPresent()) {
+						
+			return ResponseEntity.ok(cashCardOptional.get());
 		}
 		else {
 			return ResponseEntity.notFound().build();
